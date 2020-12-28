571d836479f12b2bc7418eae69c16bed70d58686
Yudin Pavel <payudin@ozon.ru>
2020-12-28 19:55:47 +0300

    [github-ci] all all update

--- core/DBController.py
+++ core/DBController.py
@@ -1,6 +1,6 @@
 from pony import orm
 from pony.orm import db_session
-from core.models import collection, query
+from core.models import collection, item
 
 
 class DBController:
@@ -9,24 +9,44 @@ class DBController:
         self.db = orm.Database()
         self.db.bind(provider='sqlite', filename=db, create_db=True)
         self.collection_model = collection(self.db, orm)
-        self.query_model = query(self.db, orm, self.collection_model)
+        self.item_model = item(self.db, orm, self.collection_model)
         self.db.generate_mapping(create_tables=True)
 
     @db_session
     def get_collections(self):
-        pass
+        collections = list(self.collection_model.select())
+        result = []
+        for col in collections:
+            items = []
+            for i in list(self.item_model.select(lambda item: item.Collection_id == col)):
+                items.append(self.get_item(i.Id))
+
+            result.append(dict(collection=col.Name, items=items))
+        return dict(collections=result)
 
     @db_session
-    def get_items_by_collection(self, id):
-        pass
+    def get_items_by_collection(self, collection_id):
+        c = list(self.collection_model.select(lambda col: col.Id == collection_id))[0]
+        items = []
+        for i in list(self.item_model.select(lambda item: item.Collection_id == c)):
+            items.append(self.get_item(i.Id))
+        return dict(collection=c.Id, items=items)
 
     @db_session
-    def get_item(self, id):
-        pass
+    def get_item(self, item_id):
+        i = list(self.item_model.select(lambda item: item.Id == item_id))
+        return dict(
+            id=i[0].Id,
+            name=i[0].Name,
+            host=i[0].Host,
+            method=i[0].Method,
+            request=i[0].Request,
+            collection=i[0].Collection_id
+        )
 
     @db_session
-    def add_query(self, name, host, method, request_body, collection_id):
-        return self.query_model(
+    def add_item(self, name, host, method, request_body, collection_id):
+        return self.item_model(
             Name=name,
             Host=host,
             Method=method,
@@ -41,9 +61,9 @@ class DBController:
         )
 
     @db_session
-    def remove_query(self, query_id):
-        q = self.query_model.select(lambda item: item.id == query_id)
-        q.delete()
+    def remove_item(self, item_id):
+        i = self.item_model.select(lambda item: item.id == item_id)
+        i.delete()
 
     @db_session
     def remove_collection(self, collection_id):
