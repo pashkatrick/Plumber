cb7160d76475ee79e458c4930c2a3eb1605fc711
Pavel Yudin <coolbatmanarrow@gmail.com>
2021-01-25 00:16:23 +0300

    Core UPDATES + electron implemention (#8)
    * [core] add main-based branch
    
    * [core] another update
    
    * [core] another different version
    
    * [core] add new method and grant updates
    
    * [core] add export feature
    
    * [core] update method_list method
    
    * [core] add add/update methods
    
    * [core] add electron part
    
    * [core] some shit
    
    Co-authored-by: Yudin Pavel <payudin@ozon.ru>

---
+++ api-client.js
@@ -0,0 +1,158 @@
+const zerorpc = require('zerorpc')
+const config = require('./config')
+
+// Create zerorpc client
+let client = new zerorpc.Client()
+
+// Connect to the zerorpc server which is run in python
+client.connect("tcp://" + config.ZERORPC_HOST
+    + ":" + config.ZERORPC_PORT)
+
+const API = {
+    test: (callback) => {
+        client.invoke("test", (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    message_template: (host, method, callback) => {
+        client.invoke("get_message_template_handler", host, method, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                console.log(error)
+                callback(result)
+            }
+        })
+    },
+    send_request: (host, method, req, callback) => {
+        client.invoke("send_request_handler", host, method, req, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    method_list: (host, callback) => {
+        client.invoke("method_list_handler", host, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    service_list: (host, callback) => {
+        client.invoke("service_list_handler", host, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    view_method_scheme: (host, method, callback) => {
+        client.invoke("view_method_scheme_handler", host, method, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    // COLLECTIONS
+    get_collections: (callback) => {
+        client.invoke("get_collections_handler", (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    add_collection: (name, callback) => {
+        client.invoke("add_collection_handler", name, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    remove_collection: (id, callback) => {
+        client.invoke("remove_collection_handler", id, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    update_collection: (object, callback) => {
+        client.invoke("update_collection_handler", object, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    // ITEMS
+    get_item: (id, callback) => {
+        client.invoke("get_item_handler", id, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    update_item: (object, callback) => {
+        client.invoke("update_item_handler", object, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    add_item: (object, callback) => {
+        client.invoke("add_item_handler", object, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    },
+    remove_item: (id, callback) => {
+        client.invoke("remove_item_handler", id, (error, result) => {
+            if (error) {
+                console.log(error)
+                return null
+            } else {
+                callback(result)
+            }
+        })
+    }
+}
+
+Object.freeze(API)
+module.exports = { API }
---
+++ config.js
@@ -0,0 +1,37 @@
+/*
+CONFIGURATION CONSTANTS
+*/
+
+/** Should debug output be printed? */
+let DEBUG = true
+
+/** Host to use for the zerorpc server */
+let ZERORPC_HOST = "127.0.0.1"
+
+/** Port to use for the zerorpc server */
+let ZERORPC_PORT = 3535
+
+/** Path to the pyinstaller build directory. */
+let PYTHON_DIST_DIR = "dist"
+
+/** Directory where the python server defined in PYTHON_SERVER resides. */
+// let PYTHON_SERVER_DIR = "server"
+// Собранная версия
+// let PYTHON_SERVER_DIR = "dist"
+// Dev версия
+let PYTHON_SERVER_DIR = "dist"
+
+/** Name of the python app running the python server. */
+// let PYTHON_SERVER = "server" // !!!!without .py-ending!!!!
+let PYTHON_SERVER = "app" // !!!!without .py-ending!!!!
+
+
+// Export the API
+module.exports = {
+    DEBUG,
+    ZERORPC_HOST,
+    ZERORPC_PORT,
+    PYTHON_DIST_DIR,
+    PYTHON_SERVER_DIR,
+    PYTHON_SERVER
+}
